e4a7c04bfeabfb643564c12f6945120d52caa0c8
Kedarnag Mukanahallipatna <kedarnag138@gmail.com>
2016-09-20 02:14:24 +0530

    [development sign_up] Adds new routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
 
-	root :to => "public/welcome#home"
+	# devise_for :users, :as => "", 
+	# 						:path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
 
-	devise_for :users, controllers: {
-           :sessions => "users/sessions",
-           :registrations => "users/registrations" }
+	devise_for :users, :controllers => {
+        confirmations: 'users/confirmations',
+        passwords: 'users/passwords',
+        registrations: 'users/registrations',
+        sessions: 'users/sessions',
+        unlocks: 'users/unlocks'
+    }
+
+	devise_scope :user do
+		get   "/users/login"             => "users/session#new",                :as => "login"
+		get   "/users/logout"            => "users/session#destroy",            :as => "logout"
+		get   "/users/new"               => "users/registrations#new",          :as => "new_user_registration"
+		post   "/users/sign_up"          => "users/registrations#create",       :as => "registration"
+	end
   
+	root :to => "public/welcome#home"
 end
