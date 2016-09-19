d701769c9e74db92c5f1b281816ed4ba74e81491
Kedarnag Mukanahallipatna <kedarnag138@gmail.com>
2016-09-20 02:12:16 +0530

    [development sign_up] Overrides the registration controller.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,15 +1,15 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-# before_action :configure_sign_up_params, only: [:create]
+before_action :configure_sign_up_params, only: [:create]
 # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   def new
-    super
+    @user = User.new
   end
 
   # POST /resource
   def create
-    super
+    
   end
 
   # GET /resource/edit
@@ -40,7 +40,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation, :designation, :status, :avatar_url])
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :employee_id, :password, :password_confirmation, :designation, :status, :avatar_url])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
@@ -49,9 +49,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    super(resource)
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
