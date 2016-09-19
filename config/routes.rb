2dd686bec152a62efdda0b6fc60dae913248c840
Kedarnag Mukanahallipatna <kedarnag138@gmail.com>
2016-09-20 00:49:03 +0530

    [development sign_up] Configures the routes for devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
 	root :to => "public/welcome#home"
-  
-  post  "/account/sign_up"          => "account/registrations#create", :as => "sign_up"
+
+	devise_for :users, controllers: {
+           :sessions => "users/sessions",
+           :registrations => "users/registrations" }
   
 end
