e094d616d5fc371539157c2168c28b75da918cc9
Kedarnag Mukanahallipatna <kedarnag138@gmail.com>
2016-09-20 02:17:13 +0530

    [development sign_up] Changes the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
-	# devise_for :users, :as => "", 
-	# 						:path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
-
 	devise_for :users, :controllers => {
         confirmations: 'users/confirmations',
         passwords: 'users/passwords',
@@ -14,7 +11,7 @@ Rails.application.routes.draw do
 	devise_scope :user do
 		get   "/users/login"             => "users/session#new",                :as => "login"
 		get   "/users/logout"            => "users/session#destroy",            :as => "logout"
-		get   "/users/new"               => "users/registrations#new",          :as => "new_user_registration"
+		get   "/users/new"               => "users/registrations#new",          :as => "new_user_signup"
 		post   "/users/sign_up"          => "users/registrations#create",       :as => "registration"
 	end
   
